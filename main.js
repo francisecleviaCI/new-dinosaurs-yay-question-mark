0066caa19950a4922ae29d09a4cbc9af8926a90e
Francis Eclevia <65243967+francisecleviaCI@users.noreply.github.com>
2020-07-22 23:58:08 -0400

    hw

--- main.js
+++ main.js
@@ -1,15 +1,89 @@
-const makeDino = function() {
-}
+const makeDino = function(species, period, carnivore, extinct) {
+    const expectedDino = {};
+    expectedDino.species = species;
+    expectedDino.period = period;
+    expectedDino.carnivore = carnivore;
+    expectedDino.extinct = extinct;
+    
+    if (extinct === undefined) {
+      expectedDino.extinct = false;
+    }
+   return expectedDino
 
-const makeSingular = function() {
-}
+    }
+const dinosaur = makeDino('Eoraptor', 'Triassic', true, false);
+console.log(dinosaur)
 
-const truncateSpecies = function() {
-}
+   
 
-const makeExtinct = function() {
+const makeSingular = function(species, period, carnivore, extinct) {
+  const dino = {};
+    dino.species = species;
+    dino.period = period;
+    dino.carnivore = carnivore;
+    dino.extinct = extinct;
+    
+    // if (species.includes('us') === true) {
+    //   dino.species.slice(species.length-2, species.length-1)
+    // } else {
+    //   dino.species = species;
+    // }
+    
+   return dino
 
-}
+    }
+const dinosaurus = makeSingular('Eoraptor', 'Jurassic', false, true);
+console.log(dinosaurus);
+
+
+
+
+const truncateSpecies = function(species, period, carnivore, extinct) {
+  const expectedDino = {};
+  expectedDino.species = species;
+  expectedDino.period = period;
+  expectedDino.carnivore = carnivore;
+  expectedDino.extinct = extinct;
+  
+
+ return expectedDino
+
+  }
+const dinosaury = truncateSpecies('Brontasaurus', 'Triassic', true, false);
+console.log(dinosaury)
+
+
+const makeExtinct = function(species, period, carnivore, extinct) {
+  const expectedDino = {};
+  expectedDino.species = species;
+  expectedDino.period = period;
+  expectedDino.carnivore = carnivore;
+  expectedDino.extinct = extinct;
+  
+  if (extinct !== undefined) {
+    expectedDino.extinct = true;
+  }
+ return expectedDino
+
+  }
+const dinosaurizzo = makeExtinct('Eoraptor', 'Triassic', true, true);
+console.log(dinosaurizzo)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = {
   makeDino,
